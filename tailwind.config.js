085f6715b5f69eab249e0a124fdf9b6c0807796b
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-11-26 21:23:05 +0530

    added provider path to content in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./providers/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
