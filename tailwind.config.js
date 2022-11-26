ccce6aee12979a8c25d1367d145ba830196aec73
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-11-26 21:14:01 +0530

    added paths to purge array in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    "./pages/**/*.js",
+    "./components/**/*.js"
+  ],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
