89ce4bdf851e1f8345532064c83e019dec4bc384
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-11-26 22:27:37 +0530

    changed paths dir - tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  purge: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./providers/**/*.{js,ts,jsx,tsx}",
+    purge: [
+      "./pages/*.{js,ts,jsx,tsx}",
+     "./components/*.{js,ts,jsx,tsx}"
   ],
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./providers/**/*.{js,ts,jsx,tsx}",
+    "./pages/*.{js,ts,jsx,tsx}",
+    "./components/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
