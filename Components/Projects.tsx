b0a4d270e9c2fa99a66e70e0638771e007fec514
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-01-16 01:55:05 +0530

    Add: Demo button to projects

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -9,41 +9,49 @@ type Props = {
 }
 
 function Projects({projects}: Props) {
-  // const handleClick = (link) =>{
-  //   windows.
-  // }
+
   return (
     <motion.div
     initial={{opacity: 0}}
     whileInView={{opacity: 1}}
     transition={{duration: 1.5}}
     className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      <h3 className='absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl z-30'>
         Projects
       </h3>
       
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#0284c7]/40'>
         {projects.map((project, i) => (
           <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-10 md:p-44 h-screen'>
-            <a href=""></a>
-            <motion.img
-            initial={{
-              y: -300,
-              opacity: 0
-            }}
-            onClick={() => window.open(`${project.linkToBuild}`, "_blank")}
-            transition={{duration: 1.2}}
-            whileInView={{opacity: 1, y: 0}}
-            viewport={{once: true}}
-            src={urlFor(project?.image).url()} 
-            alt="Project Image Icon"
-            className='h-[200px] w-[300px] rounded-xl shadow-2xl hover:cursor-pointer'
-            />
+            
+              <motion.img
+              initial={{
+                y: -300,
+                opacity: 0
+              }}
+              transition={{duration: 1.2}}
+              whileInView={{opacity: 1, y: 0}}
+              viewport={{once: true}}
+              src={urlFor(project?.image).url()} 
+              alt="Project Image Icon"
+              className='h-[200px] w-[300px] rounded-xl shadow-2xl hover:cursor-pointer'
+              />
+            <div className='flex gap-3'>
+              <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => window.open(`${project.linkToBuild}`, "_blank")}                
+              >
+                Demo
+              </button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Source Code
+              </button>
+            </div>
 
             <div className='space-y-10 px-10 md:px-10 max-w-6xl'>
               <h4 className='text-lg font-semibold text-center md:text-4xl'>
                 <span className='uderline decoration-[#0ea5e9]/50'>
-                  Demo {i + 1} of {projects.length}:
+                  {i + 1} of {projects.length}:
                 </span>{" "}
                 {project?.title}
               </h4>
