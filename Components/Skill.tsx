5de96037fe95ade2fbdc2bb9f9db1934929b8a83
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-01-16 00:03:40 +0530

    Style: fixed logo size for skills page

--- Components/Skill.tsx
+++ Components/Skill.tsx
@@ -14,18 +14,18 @@ function Skill({skill, directionLeft}: Props) {
     <div className='group relative flex cursor-pointer'>
         <motion.img
         initial={{
-            x: directionLeft ? -200 : 200,
+            x: directionLeft ? -100 : 100,
             opacity: 0
         }}
         transition={{duration: 1}}
         whileInView={{opacity: 1, x: 0}}
         alt={'Skill Image'}
         src={urlFor(skill?.image).url()}
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group:hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 object-cover w-[40px] h-[40px] md:w-[70px] md:h-[70px] xl:w-[80px] xl:h-[80px] filter group:hover:grayscale transition duration-300 ease-in-out"
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duraition-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full'>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duraition-300 ease-in-out group-hover:bg-white w-[40px] h-[40px] md:w-[70px] md:h-[70px] xl:w-[80px] xl:h-[80px] rounded-full'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
+                <p className='text-[12px] md:text-[20px] font-bold text-black opacity-100'>{skill.progress}%</p>
             </div>
         </div>
     </div>
--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -21,7 +21,7 @@ function Skills({skills}: Props) {
             Hover over a skill for current proficiency
         </h3>
 
-        <div className='grid grid-cols-4 gap-5'>
+        <div className='grid grid-cols-3 gap-5'>
           {skills?.slice(0, skills.length / 2).map((skill) => (
             <Skill key={skill._id} skill={skill}/>
           ))}
