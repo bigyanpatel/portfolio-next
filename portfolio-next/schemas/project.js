c575e38f2af58480d638c67e905ae6d8aaafaf19
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-01-16 19:27:15 +0530

    Refactor: Added linkToCode in project schemas

--- portfolio-next/schemas/project.js
+++ portfolio-next/schemas/project.js
@@ -32,6 +32,11 @@ export default {
       name: "linkToBuild",
       title: "LinkToBuild",
       type: "url"
+    },
+    {
+      name: "linkToCode",
+      title: "LinkToCode",
+      type: "url"
     }
   ],
 }
